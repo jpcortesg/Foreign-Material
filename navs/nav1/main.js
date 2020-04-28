1b1763b85dc04a0e9860d49f76ccf7b70d3b16a7
jpcortesg <juan2001cortes.jc@gmail.com>
2020-04-28 12:07:25 -0500

    Cambios en la barra de navegación

--- navs/nav1/main.js
+++ navs/nav1/main.js
@@ -1,33 +1,21 @@
-var $els = $('.menu a, .menu header');
-var count = $els.length;
-var grouplength = Math.ceil(count / 3);
-var groupNumber = 0;
-var i = 1;
-$('.menu').css('--count', count + '');
-$els.each(function (j) {
-  if (i > grouplength) {
-    groupNumber++;
-    i = 1;
-  }
-  $(this).attr('data-group', groupNumber);
-  i++;
-});
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
 
-$('.menu footer button').on('click', function (e) {
-  e.preventDefault();
-  $els.each(function (j) {
-    $(this).css('--top', $(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
-    $(this).css('--delay-in', j * .1 + 's');
-    $(this).css('--delay-out', (count - j) * .1 + 's');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if(link.style.animation){
+        link.style.animation = ``;
+      }else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    });
+
+    burger.classList.toggle('toggle');
   });
-  $('.menu').toggleClass('closed');
-  e.stopPropagation();
-});
+}
 
-// run animation once at beginning for demo
-setTimeout(function () {
-  $('.menu footer button').click();
-  setTimeout(function () {
-    $('.menu footer button').click();
-  }, (count * 100) + 500);
-}, 1000);
+navSlide();
