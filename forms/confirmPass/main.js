9864150feee57f163708005b257373791ba23327
jpcortesg <juan2001cortes.jc@gmail.com>
2020-06-06 03:34:42 -0500

    forms and animations

---
+++ forms/confirmPass/main.js
@@ -0,0 +1,4 @@
+const pswrd_1 = document.querySelector('#pswrd_1');
+const pswrd_2 = document.querySelector('#pswrd_2');
+const errorText = document.querySelector('.error-text');
+const btn = document.querySelector('.Button');
