f1c29f2e28649b8f16074bdcab2d6d5cf01d0c0e
jpcortesg <juan2001cortes.jc@gmail.com>
2020-08-03 18:37:01 -0500

    change in the names in the folders

--- secondAttempt/js/main.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.nav_burger');
-const items = document.querySelector('.nav_items');
-const burger_btn = document.querySelector('.nav_burger_btn');
-
-let active = false;
-
-burger.addEventListener('click', activeFun);
